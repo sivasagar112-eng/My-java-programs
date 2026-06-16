ddf7e308eaa18542a38973cfd4c85d8e86a768b4
Siva <sivasagar@gmail.com>
2026-06-16 10:04:29 +0530

    .

--- multilevel_inheritance.java
+++ multilevel_inheritance.java
@@ -1,24 +1,26 @@
-class Animal{
-    void eat(){
+class Animal {
+    void eat() {
         System.out.println("The animal eats food.");
     }
 }
-class Dog extends Animal{
-    void barks(){
+
+class Dog extends Animal {
+    void barks() {
         System.out.println("The dog barks.");
     }
 }
-class puppy extends Dog{
+
+class puppy extends Dog {
     void weep(){
         System.out.println("The puppy weeps.");
     }
 }
 
 public class multilevel_inheritance {
-    public static void main(String args[]){
+    public static void main(String args[]) {
         puppy Mypuppy = new puppy();
-            Mypuppy.eat();
-            Mypuppy.barks();
-            Mypuppy.weep();
+        Mypuppy.eat();
+        Mypuppy.barks();
+        Mypuppy.weep();
     }
 }
