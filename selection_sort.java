4d31f8160efc9155ce1fc49d4f0e177a5c9a4a66
Siva <sivasagar@gmail.com>
2026-06-19 12:47:41 +0530

    Sorting practice

---
+++ selection_sort.java
@@ -0,0 +1,22 @@
+public class selection_sort {
+    public static void PrintArray(int arr[]){
+        for(int i = 0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        int arr[] = {7,8,3,1,2};
+        for(int i = 0;i<arr.length-1;i++){
+            int small = i;
+            for(int j = i+1;j<arr.length;j++){
+                if (arr[small] > arr[j]) {
+                    small = j;
+                }
+            }
+            int temp = arr[small];
+            arr[small] = arr[i];
+            arr[i] = temp;
+        }
+        PrintArray(arr);
+    }
+}
